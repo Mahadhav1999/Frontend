60616ea0be2ebe4c0940a85e8c3aa4f9ca0669c1
Mahadhav Dhobale <mahadhavdhobale@gmail.com>
2023-05-11 12:16:48 +0530

    changes

--- src/components/pages/Posts/Posts.jsx
+++ src/components/pages/Posts/Posts.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Banner from '../../Common/Banner';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 const Posts = () => {
     var name = 'All Posts';
 
     const [posts, setPosts] = React.useState([])
-    const navigate = useNavigate()
 
 
     let fetchPosts = async () => {
